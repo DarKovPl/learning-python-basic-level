08d0b5ff5be5e26482f105cc9f52f8b69d1d5b10
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-03-19 21:39:58 +0100

    Added Text file and modified Datetime file

--- Datetime.py
+++ Datetime.py
@@ -1,2 +1,4 @@
+print("Hello, what day is it today?")
 from datetime import date
-print(date.today())
+print("Today it is:")
+print(date.today().strftime("%A"))
---
+++ Text.py
@@ -0,0 +1,22 @@
+# Python isa case sensitive
+text = 'This is a text'
+print(text)  #simple assignment here
+anotherText = 'this is a text'
+print(text == anotherText)
+
+
+''' 
+This is a block comment
+        I can add here more comments here
+'''
+print("Task: ")
+# Amount eye winks per minute
+blinksPerMin = 20
+# Amount of minutes per hour, and amount of hours per twenty four hours
+minInHour  = 60
+hoursInDay = 24
+daysInYear = 365
+# Amount of years
+years = 50
+# Amount of winks per X years
+print(blinksPerMin*minInHour*hoursInDay*daysInYear*years)
