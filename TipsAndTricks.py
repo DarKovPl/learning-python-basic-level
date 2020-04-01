783e90049049ecb32457ec177822c9992e9714db
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-04-01 20:14:37 +0200

    New file was added: TipsAndTrics

---
+++ TipsAndTricks.py
@@ -0,0 +1,45 @@
+title = "Python"
+print('Title is', type(title))
+
+version = 3
+print('Version is', type(version))
+
+progress = 0.21
+print('Progres is', type(progress))
+
+print('This expression is', type(progress * version))
+
+x = 1
+y = 1
+z = 1
+print(x, y, z)
+
+a = b = c = 2
+print(a, b, c)
+
+c = 3
+print(a, b, c)
+print(2+2*2)
+print(6/2*(1+2))
+print(4**3**2)
+
+x = 1
+print(x)
+x += 1
+print(x)
+
+x -= 1
+print(x)
+
+# Laboratory
+
+v1 = 126
+v2 = '126'
+v3 = 126.0
+v4 = '126.0'
+print(type(v1), type(v2), type(v3), type(v4))
+print(type(v1+v3))
+print(type(v2+v4))
+print(7-1*0+3+3)
+print(4**5/2**3)
+print(99+9/9)
