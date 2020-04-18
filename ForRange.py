486276ed6a8e8dc93c236691c630bc525cdfea41
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-04-18 19:08:19 +0200

    New file was added: ForRange

---
+++ ForRange.py
@@ -0,0 +1,49 @@
+for number in range(1, 21):
+
+    if number % 2 == 0:
+        print('Number %2d is %s' % (number, 'even'))
+    else:
+        print('Number %2d is %s' % (number, 'odd'))
+print('----------------------------------------------------------')
+
+# Laboratory
+
+string_A = '+---+---+---+---+'
+string_B = '|   |   |   |   |'
+
+for A in range(10):
+    print(string_A, '\tLoop: %d' % (A + 1))
+
+print('----------------------------------------------------------')
+
+for A in range(5):
+    print(string_A, '\tMain loop: %d' % A)
+
+    if A == 4:
+        break
+    for B in range(1):
+        print(string_B)
+
+print('-----------------------------------------------------------')
+
+for X in range(1, 10):
+    print('x' * X)
+
+print('-----------------------------------------------------------')
+
+for O in range(1, 10):
+
+    if O % 2 == 1:
+        print('O' * O)
+    else:
+        print('X' * O)
+print('-----------------------------------------------------------')
+
+for O in range(1, 10, 2):
+    print('O' * O)
+
+    if O == 9:
+        break
+
+    for X in range(1):
+        print('x' * (O+1))
