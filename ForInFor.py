109655cee8309105c72c31b1b279c7b698e9e22b
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-04-20 20:52:36 +0200

    New file was added: Break

--- ForInFor.py
+++ ForInFor.py
@@ -13,25 +13,10 @@ for f in range(i):
     print('{0:1d}!\t= {1:7d}'.format(f + 1, factorial))
 print('-----------------------------------------------------------')
 
-i = 11
-textListFactorial = []
-z = textListAddNumbersForMultiple = []
-characters = ['{}']
-
-for f in range(i):
-
-    factorial = 1
-    textListFactorial.append(f)
-
-    for f2 in range(f):
-
-        factorial *= (f2 + 1)
-        textListAddNumbersForMultiple.append(f2+1)
-
-        print(f, f2 + 1, factorial)
-
-    print('{0:2d}!\t= '.format(textListFactorial.pop(0), ))
-print(textListAddNumbersForMultiple)
-
-list_noun = ['dog', 'potato', 'meal', 'icecream', 'car']
+list_noun = ['dog', 'potato', 'meal', 'ice cream', 'car']
 list_adj = ['dirty', 'big', 'hot', 'colorful', 'fast']
+
+for noun in range(len(list_noun)):
+
+    for adj in range(len(list_adj)):
+        print(list_adj[adj], list_noun[noun])
