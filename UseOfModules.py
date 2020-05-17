400e805ac497b44a5b13385a8ee597db8168467b
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-05-17 21:41:42 +0200

    One file was added: UseOfModules

---
+++ UseOfModules.py
@@ -0,0 +1,15 @@
+import math
+
+print(math.pi)
+
+print(math.floor(math.pi))
+print('-----------------------------------------------------')
+
+from math import *
+
+print(pi)
+
+print(floor(pi))
+print('-----------------------------------------------------')
+
+# Laboratory
