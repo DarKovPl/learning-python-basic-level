55e0a0839e5f08c7cb89163efedc452139ccde3e
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-05-18 21:29:43 +0200

    One file  was added: MathModule

---
+++ MathModule.py
@@ -0,0 +1,24 @@
+import math
+
+f_smaller = 3.141592653589793
+f_bigger = 3.87756392764
+
+print(math.ceil(f_smaller), math.ceil(f_bigger), '\n')  # zwraca najmniejszą liczbe całkowitą która jest większa od przekazanego argumetu
+
+print(math.floor(f_smaller), math.floor(f_bigger), '\n')  # zwraca największą liczbe całkowitą która jest mniejsza od przekazanego argumentu
+
+print(math.ceil(-f_smaller), math.ceil(-f_bigger), '\n')
+
+print(math.floor(-f_smaller), math.floor(-f_bigger), '\n')
+
+print(pow(2, 8), pow(9, 0.5), '\n')  # Potęgi, Pierwastki
+
+print(math.sqrt(16), '\n')  # Pierwiastek kwadratowy
+
+print(math.pi, math.e, '\n')  # Zmienne stałe
+
+print(math.sin(math.pi/2), math.cos(math.pi/4))
+print('-----------------------------------------------------')
+
+# Laboratory
+
