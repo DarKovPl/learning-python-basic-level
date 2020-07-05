5066704db7ca21310738bdd6673948eb7d398175
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-07-05 22:00:53 +0200

    One file was modified: OsModule

--- OsModule.py
+++ OsModule.py
@@ -19,3 +19,16 @@ print('The directory part is: ', os.path.dirname(file_path))
 print('------------------------------------------------------------')
 
 print('\nIs file existing? ', os.path.exists(file_path))
+print('------------------------------------------------------------')
+
+if os.path.exists(file_path):
+
+    print('\nLast modify date is: ', os.path.getmtime(file_path))
+    print('\nLast modify date is: ', time.localtime(os.path.getmtime(file_path)))
+    print('\nFile size is: ', os.path.getsize(file_path))
+    print('\nIs it a dir? ', os.path.isdir(file_path))
+    print('\nIs it a file? ', os.path.isfile(file_path))
+    print('\nPath splitted: ', os.path.split(file_path))
+    print('\nOnly file name part: ', os.path.split(file_path)[1])
+    print('\nPath splitted into drive: ', os.path.splitdrive(file_path))
+    print('Only drive: ', os.path.splitdrive(file_path)[0])
