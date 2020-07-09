dbaadc083870c76722a108c9db3655ee580b5cd3
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-07-09 22:07:51 +0200

    One file was modified: CheckingPathToTheFile

--- CheckingPathToTheFile.py
+++ CheckingPathToTheFile.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+import shutil
 
 file_is_ok = False
 
@@ -14,13 +16,55 @@ file_is_ok = False
 #         print('Name of the file is invalid. Try again.')
 print('------------------------------------------------------------')
 
-while True:
+# while True:
+#
+#     file_name = input('Enter path to results.txt file: ')
+#
+#     if os.path.isfile(file_name):
+#         print('The results file is %s' % file_name)
+#         break
+#
+#     else:
+#         print('Name of the file is invalid. Try again.')
+print('------------------------------------------------------------')
 
-    file_name = input('Enter path to results.txt file: ')
+# Laboratory
 
-    if os.path.isfile(file_name):
-        print('The results file is %s' % file_name)
-        break
+data_input_catalog = r'/tmp/data_input'
+data_output_catalog = r'/tmp/data_output'
+today = datetime.date.today()
+
+today_output_catalog = os.path.join(data_output_catalog, today.strftime("%Y-%m-%d"))
+today_input_catalog = os.path.join(data_input_catalog, today.strftime("%Y-%m-%d"))
+
+yes = 'Y'
+
+if not os.path.isdir(data_input_catalog):
+    os.makedirs(today_input_catalog)
+
+else:
+    if yes == input("This directory: %s is already existing, if you want to delete it,"
+                    " input 'y' here. ->: " % today_input_catalog).upper():
+        shutil.rmtree(os.path.split(today_input_catalog)[0])
+        print("Run the program again to create a new catalog with today's date.")
 
     else:
-        print('Name of the file is invalid. Try again.')
+        print("New catalog %s wasn't created because there was already a catalog with the same name,"
+              " and the old one wasn't deleted." % today_input_catalog)
+
+
+if not os.path.isdir(today_output_catalog):
+    os.makedirs(today_output_catalog)
+
+else:
+    if yes == input("This directory: %s is already existing, if you want to delete it,"
+                    " input 'y' here. ->: " % today_output_catalog).upper():
+        shutil.rmtree(os.path.split(today_output_catalog)[0])
+        print("Run the program again to create a new catalog with today's date.")
+
+    else:
+        print("New catalog %s wasn't created because there was already a catalog with the same name,"
+              " and the old one wasn't deleted." % today_output_catalog)
+
+today_output_catalog = os.path.join(data_output_catalog, today.strftime("%Y-%m-%d"))
+is_input_catalog_ok = os.path.isdir(data_input_catalog)
