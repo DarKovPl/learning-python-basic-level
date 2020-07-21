40cb631336fd272ec0b80c16f1e6698c024b403d
Dariusz Kowalczyk <dariusz_kowalczykk@wp.pl>
2020-07-21 21:03:30 +0200

    One file was modified: ErrorHandlingTryInstruction

--- ErrorHandlingTryInstruction.py
+++ ErrorHandlingTryInstruction.py
@@ -1,15 +1,42 @@
 import sys
 
-tasks_per_person = 0
+# tasks_per_person = 0
+#
+# try:
+#     tasks = 32
+#     persons_string = input('How many persons are there in the team? ')
+#     persons = int(persons_string)
+#
+#     tasks_per_person = tasks / persons
+#
+# except:
+#     print('Something went wrong.....', sys.exc_info()[0])
+#
+# print("Every person should have around", tasks_per_person, ' tasks.')
 
-try:
-    tasks = 32
-    persons_string = input('How many persons are there in the team? ')
-    persons = int(persons_string)
+#  Laboratory
 
-    tasks_per_person = tasks / persons
 
-except:
-    print('Something went wrong.....', sys.exc_info()[0])
+file_path = r'/tmp/orders.csv'
 
-print("Every person should have around", tasks_per_person, ' tasks.')
+with open(file_path, "r") as file:
+
+    for line in file:
+
+        line = line.replace('\n', '')
+        order = line.split(',')
+
+        try:
+            pharmacy_name = order[0]
+            item = order[1]
+            amount = int(order[2])
+
+            print('Order from drugstore %s, item %s, amount %d' % (pharmacy_name, item, amount))
+
+        except:
+            print('------------')
+            print("Something wrong in line: %d." % sys.exc_info()[-1].tb_lineno,
+                  'Information of an exception: %s' % sys.exc_info()[0])
+            print('************')
+
+print("Processing finished")
